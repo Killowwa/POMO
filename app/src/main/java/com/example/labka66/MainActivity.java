530495ada917cba70e0a359132a3a88512b5b58b
Killowa <serikakhmetovadil@mail.ru>
2025-03-27 11:08:20 +0500

    ggogo

--- app/src/main/java/com/example/labka66/MainActivity.java
+++ app/src/main/java/com/example/labka66/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.labka66;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    CountryAdapter countryAdapter;
+    List<Country> countryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Инициализация списка стран
+        countryList = new ArrayList<>();
+        countryList.add(new Country("China", "china", 1408000000));
+        countryList.add(new Country("United States", "usa", 340000000));
+        countryList.add(new Country("Russia", "russia", 146000000));
+
+        // Создаем адаптер и передаем обработчик кликов
+        countryAdapter = new CountryAdapter(this, countryList, new CountryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Country country) {
+                // Показать Toast с информацией о выбранной стране
+                String message = "Selected: " + country.getCountryName() +
+                        " (Population: " + country.getPopulation() + ")";
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
         });
+
+        recyclerView.setAdapter(countryAdapter);
     }
 }
